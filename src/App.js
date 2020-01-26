10535d13cccaabbe1d20fd7701cd144ff2d4b845
johperry <jack.perry@du.edu>
2020-01-26 07:22:35 -0700

    broken homepage into smaller components, finished styling menu-item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './Homepage';
+import HomePage from './Pages/Homepage/Homepage';
 
 function App() {
   return (
---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Homepage.scss';
+import Directory from '../../Components/Directory/Directory';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory /> 
+    </div>
+);
+
+export default HomePage;
