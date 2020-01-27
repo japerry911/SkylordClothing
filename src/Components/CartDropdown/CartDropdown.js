0612daf69cbb286cbf66d6e4abe1de176dd47838
johperry <jack.perry@du.edu>
2020-01-27 05:27:07 -0700

    added in reselect library and cart selectors redux

--- src/Components/CartDropdown/CartDropdown.js
+++ src/Components/CartDropdown/CartDropdown.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './CartDropdown.scss';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
+import { connect } from 'react-redux';
+import { selectCartItems } from '../../Redux/Cart/CartSelectors';
 
-const CartDropdown = ({ hidden }) => (
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state) 
+});
+
+export default connect(mapStateToProps)(CartDropdown);
