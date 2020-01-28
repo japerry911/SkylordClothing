3050ccbe146484eb967fcbd124125aacf9777dce
johperry <jack.perry@du.edu>
2020-01-28 08:59:46 -0700

    updating stripe integration, adding test card info, updating favicon.ico

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../Redux/Cart/CartSelectors';
 import CheckoutItem from '../../Components/CheckoutItem/CheckoutItem';
+import StripeCheckoutButton from '../../Components/StripeButton/StripeButton';
 
 const CheckoutPage = ({ cartItems, cartTotal }) => (
     <div className='checkout-page'>
@@ -33,6 +34,14 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
         <div className='total'>
             <span>TOTAL: ${cartTotal}</span>
         </div>
+
+        <div className='test-warning'>
+            *** Please use the following test credit card for payments ***
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        
+        <StripeCheckoutButton price={cartTotal} />
     </div>
 );
 
