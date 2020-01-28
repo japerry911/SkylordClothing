0d2256462d856347a93de4491a0a4c8e4abce754
johperry <jack.perry@du.edu>
2020-01-27 21:41:22 -0700

    directoryReducer added to hold sections in redux;

--- src/Redux/RootReducer.js
+++ src/Redux/RootReducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from 'redux';
 import userReducer from './User/UserReducer';
 import cartReducer from './Cart/CartReducer';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import directoryReducer from './Directory/DirectoryReducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer)
